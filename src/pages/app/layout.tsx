326bd7a822a2efc2b685ab40ab3ea7d462c3b30d
mizi.lin <mizi.lin@mizohen.com>
2024-05-05 23:11:50 +0800

    feat: 如意 logo

--- src/pages/app/layout.tsx
+++ src/pages/app/layout.tsx
@@ -8,7 +8,12 @@ export const Layout: FC = () => {
     return (
         <article className={styles.layout}>
             <header className={styles.header}>
-                <div className={styles.logo}>如意</div>
+                <div className={styles.logo} style={{ background: `url(${chrome.runtime.getURL('ruyi-32.png')}) no-repeat left center` }}>
+                    <Space>
+                        <span style={{ fontSize: 14 }}>Ruyi</span>
+                        <span>如意</span>
+                    </Space>
+                </div>
                 <div></div>
             </header>
             <main className={styles.main}>
